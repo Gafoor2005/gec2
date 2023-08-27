5a937a71d84a54b6affaf574a74b94d879631182
Gafoor <gafoorma686@gmail.com>
2023-08-27 20:42:29 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import { useState } from 'react';
+import Login from './pages/Login';
 
 function App() {
+  const [loginStatus,setLoginStatus] = useState(false)
+  const [uid,setUid] = useState(undefined)
+  function Credentials(id,pass){  // validation code here
+    // console.log("id is",id,"pass is",pass);
+    setUid(id)
+    return Boolean(id)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home loginStatus={loginStatus} />}/>
+          <Route exact path='/about' element={<About/>}/>
+          <Route exact path='/login' element={Login(Credentials)}/>
+        </Routes>
+      </Router>
+      <p>App</p>
+      <button onClick={()=>{setLoginStatus((e)=>!e)}}>btn</button>
+    </>
   );
 }
+
+
 
 export default App;
